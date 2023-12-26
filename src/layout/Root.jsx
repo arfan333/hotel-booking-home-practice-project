027a87f34a15b8b72cace556a94ac82a221fb52d
arfan333 <arfantamim333@gmail.com>
2023-12-26 23:43:43 +0600

    page & components setup

---
+++ src/layout/Root.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h1>hello pakistan</h1>
+        </div>
+    );
+};
+
+export default Root;
